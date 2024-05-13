058445bc4998710c0f340d18592a1748498a1f15
Mateo <mateom@wowzi.co>
2024-05-13 20:09:54 +0200

    e2e tests with Playwright.

---
+++ playwright/register.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Register page: new account is created correctly', async ({ page }) => {
+  await page.goto('/register');
+
+  await page.getByPlaceholder('Añade tu email').fill('thisisanewaccount4@gmail.com');
+  await page.getByPlaceholder('Añade tu nombre').fill('accountname4');
+  await page.getByRole('button', { name: 'Siguiente' }).click();
+  await page.getByPlaceholder('Password').fill('pwd654321');
+  
+  const responsePromise = page.waitForResponse('**/auth/signup');
+  await page.getByRole('button', { name: 'Finalizar' }).click();
+
+  const response = await responsePromise;
+  await expect(response.status()).toBe(201);
+});
